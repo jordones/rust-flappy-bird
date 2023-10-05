607014d091a1bf1d0077b08e1ae1df34cb760218
jordones <jordon.e.smith@gmail.com>
2023-10-05 12:38:20 -0400

    Implement Bird as custom bundle, reorganize

--- src/bird.rs
+++ src/bird.rs
@@ -1,21 +1,42 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-pub struct Bird();
-
 pub struct BirdPlugin;
 
 impl Plugin for BirdPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_bird);
+        app
+            .add_systems(Startup, spawn_bird)
+            .add_systems(Update, apply_gravity_to_bird);
     }
+}
+
+#[derive(Component)]
+struct Bird;
+
+#[derive(Bundle)]
+struct BirdBundle {
+    marker: Bird,
+    sprite: SpriteBundle,
 }
 
 fn spawn_bird(mut commands: Commands, asset_server: Res<AssetServer>) {
     let texture = asset_server.load("bird.png");
 
-    commands.spawn(SpriteBundle {
-        texture,
-        ..default()
+    commands.spawn(BirdBundle {
+        marker: Bird,
+        sprite: SpriteBundle {
+            texture,
+            ..default()
+        }
     });
+
+}
+
+fn apply_gravity_to_bird(
+    mut query: Query<(&mut Transform, With<Bird>)>,
+    time: Res<Time>
+) {
+    let mut bird_transform = query.single_mut();
+
+    bird_transform.0.translation.y -= time.delta_seconds() * 60.0;
 }
